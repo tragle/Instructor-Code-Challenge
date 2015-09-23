20e175a81b60bc68eacf6edf84df87fcc793a835
Tom Ragle <tragle@avaya.com>
2015-09-23 19:43:36 -0400

    Adding template support. Not parsing JSON in ajax function anymore since result may now be HTML.

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 var Client = Client || function() {
 
-    var API_URL = "http://www.omdbapi.com/?";
+    // Debug
     
     function log(message) {
         /* Logs messages to debug console */
@@ -8,6 +8,12 @@ var Client = Client || function() {
             console.log(message);
         } // else ignore if console unsupported
     }
+
+    // API
+    
+    var API_URL = "http://www.omdbapi.com/?",
+        TEMPLATE_URL = "template.html",
+        ROOT_SELECTOR = "#app";
 
     function ajax(url, callback) {
         /* Send AJAX GET to URL and pass result to callback */
@@ -18,7 +24,7 @@ var Client = Client || function() {
         request.onload = function() { // Supporting only IE9+ to avoid more boilerplate
             if (request.status >= 200 && request.status < 400) {
                 // Successful request + response
-                var data = JSON.parse(request.responseText);
+                var data = request.responseText;
                 callback(data);
             } else {
                 // TODO: use onError callback?
@@ -38,6 +44,23 @@ var Client = Client || function() {
         var query = "s=" + title;
         ajax(API_URL + query, callback);
     }
+
+    // DOM
+
+    function loadTemplate(selector, callback) {
+        /* Inject template html into selector, passes reference to resulting DOM element to callback */
+        var $root = document.querySelectorAll(selector)[0] || document.body; // default to body if no matching elems
+       
+        ajax(TEMPLATE_URL, function(html) {
+            $root.innerHTML = html; // TODO: Make this safe
+            if (typeof callback === "function") {
+                callback($root);
+            }
+        });
+    }
+
+    loadTemplate(ROOT_SELECTOR); // Paint the template
+
 
 }();
 
