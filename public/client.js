9c275ea81423274937110105e05da6c9d4888c30
Tom Ragle <tragle@avaya.com>
2015-09-23 16:07:32 -0400

    Adding client.js and simple console logging function

---
+++ public/client.js
@@ -0,0 +1,16 @@
+var Client = Client || function() {
+
+    function log(message) {
+        if (window.console && window.console.log) {
+            console.log(message);
+        }
+    }
+
+    log("Client loaded.");
+
+}();
+
+
+    
+
+
