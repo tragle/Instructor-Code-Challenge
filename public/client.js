e3973ac544f373bab38ede164ddca2d6605864fb
Tom Ragle <tragle@avaya.com>
2015-09-23 17:14:42 -0400

    Adding AJAX code and API search

--- public/client.js
+++ public/client.js
@@ -1,12 +1,43 @@
 var Client = Client || function() {
 
+    var API_URL = "http://www.omdbapi.com/?";
+    
     function log(message) {
-        if (window.console && window.console.log) {
+        /* Logs messages to debug console */
+        if (window.console && window.console.log) { // ignore if console unsupported
             console.log(message);
         }
     }
 
-    log("Client loaded.");
+    function ajax(url, callback) {
+        /* Send AJAX GET to URL and pass result to callback */
+        
+        var request = new XMLHttpRequest(); 
+        request.open('GET', url, true);
+
+        request.onload = function() { // Supporting only IE9+ to avoid more boilerplate
+            if (request.status >= 200 && request.status < 400) {
+                // Successful request + response
+                var data = JSON.parse(request.responseText);
+                callback(data);
+            } else {
+                // TODO: use onError callback?
+                throw new Error("Unable to complete AJAX request due to server error");
+            }
+        };
+
+        request.onerror = function() {
+            throw new Error("Unable to complete AJAX request due to connection error");
+        };
+        
+        request.send();
+    }
+
+    function apiSearch(title, callback) {
+        /* Search the database for movie title and pass result to callback */
+        var query = "s=" + title;
+        ajax(API_URL + query, callback);
+    }
 
 }();
 
